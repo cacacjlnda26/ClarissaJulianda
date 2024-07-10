191d99bf8ee08e12850bca0f0d981df86f3cddf0
cacacjlnda26 <caca26.cj@gmail.com>
2024-07-10 07:56:51 +0700

    Add files via upload

---
+++ qgis2web_2024_07_10-07_11_50_271888/data/JangkauanLayananKesehatan_2.js
@@ -0,0 +1,1 @@
+var json_JangkauanLayananKesehatan_2 = {"type":"FeatureCollection","name":"JangkauanLayananKesehatan_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
